2aabc48e8b93fbd29ee727910298370af96225b4
Narteno <49160330+Narteno@users.noreply.github.com>
2019-10-23 23:27:20 +0700

    my pulled

---
+++ lesson6/lesson6/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lesson6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(417, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FOR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(435, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 507);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(417, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "WHILE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(417, 61);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(417, 61);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(417, 60);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Четные числа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(27F, 54F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 531);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lesson6/lesson6/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ""; // Очищение richTextBox1
+            // for имеет три параметра
+            // 1. Начальное значение счетчика цикла
+            // 2. Условие
+            // 3. Увеличение счетчика цикла
+            for (int i = 0; i < 10; i++)
+            {
+                richTextBox1.Text += "i=" + i.ToString() + "\n";
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ""; // Очищение richTextBox1
+            // while имеет 1 параметр
+            // Начальное значение счетчика цикла задается ДО ЦИКЛА
+            // 1. Условие
+            int i = 5;
+            while (i < 20)
+            {
+                richTextBox1.Text += "i=" + i.ToString() + "\n";
+                i += 2;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ""; // Очищение richTextBox1
+            int left = Convert.ToInt32(textBox1.Text),
+                right = Convert.ToInt32(textBox2.Text),
+                summa = 0;
+            for (int i = left; i < right; i++)
+            {
+                if (i%2 == 0) // Если число делится на 2 без остатка, то оно четное
+                {
+                    richTextBox1.Text += "i=" + i.ToString() + "\n";
+                    summa += i;
+
+                }
+            }
+            MessageBox.Show(Convert.ToString(summa));// или summa.ToString()
+        }
+    }
+}
